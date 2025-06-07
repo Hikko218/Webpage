0fce9bdffcb8918dfc44088d213d9ec70ad3385a
Hikko218 <heikor93@outlook.de>
2025-06-07 23:06:37 +0200

    new design in process

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
+
+ // addRecommendation function
+
 function addRecommendation() {
+
   // Get the message of the new recommendation
+
   let recommendation = document.getElementById("new_recommendation");
   let email = document.getElementById("email");
   let name = document.getElementById("name");
+
   // If the user has left a recommendation, display a pop-up
+
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     showPopup(true);
 
     // Create a new 'recommendation' element and set it's value to the user's message
+
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.innerHTML = "\<span\>''\</span\>" + recommendation.value + "\<span\>''\</span\>";
+
     // Add this element to the end of the list of recommendations
+
     document.getElementById("all_recommendations").appendChild(element); 
     
     // Reset the value of the textarea
+
     recommendation.value = "";
     email.value = "";
     name.value = "";
   }
 }
+
+    // popup function
 
 function showPopup(bool) {
   if (bool) {
